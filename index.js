7cd1fc79e74a83d54ecd4ce2a0b0a36b1797599b
gary <gary.white@lodgit.com>
2022-02-09 12:41:56 +0100

    code

---
+++ index.js
@@ -0,0 +1,7 @@
+function well(x){
+
+}
+
+console.log()well(['bad', 'bad', 'bad']); // 'Fail!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad'])); // 'Publish!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good'])); // 'I smell a series!'
